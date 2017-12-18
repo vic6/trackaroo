82540c89c71d552de3a3683d433870e77b019009
vic6 <vicmatthews116@gmail.com>
2017-12-18 01:23:56 -0800

    Edit routes

--- src/components/EditExpense.jsx
+++ src/components/EditExpense.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const EditExpense = () => <h1>Edit expense page</h1>;
+const EditExpense = (props) => {
+  return (
+    <h1>Edit expense page {props.match.params.id}</h1>
+  )
+}
 
 export default EditExpense;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ExpenseDashboard from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
-import HelpPage from '../components/HelpPage';
+import ExpenseDashboard from '../components/ExpenseDashboard';
 import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
 import NotFound from '../components/NotFound';
 
 const AppRouter = () => (
@@ -14,7 +14,7 @@ const AppRouter = () => (
       <Switch>
         <Route exact path="/" component={ExpenseDashboard} />
         <Route path="/create" component={AddExpense} />
-        <Route path="/edit" component={EditExpense} />
+        <Route path="/edit/:id" component={EditExpense} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFound} />
       </Switch>
